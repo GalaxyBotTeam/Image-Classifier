f7afa29d87dd66f20ca9191eeaf3d61a91af511e
Phyroks <mika@phyroks.de>
2025-04-08 00:18:52 +0200

    Added Status Ping

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ import {WebServer} from './WebServer';
 import config from "../config.json";
 import {Utils, logLevel, logModule} from "./Utils/Utils";
 import {MinIO} from "./Utils/MinIO";
+import axios from "axios";
 
 async function start() {
     Utils.log(logLevel.INFO, logModule.GALAXYBOT, "Starting GalaxyBot V4 - NSFW Classifier");
@@ -13,8 +14,15 @@ start().then(() => {
     // Ready for Pterodactyl JS Egg
     console.log("Ready!")
     Utils.log(logLevel.SUCCESS, logModule.GALAXYBOT, "GalaxyBot V4 - NSFW Classifier started");
+
+    setInterval(async () => {
+        await status();
+    }, config.status.interval)
 }).catch((e) => {
     Utils.log(logLevel.ERROR, logModule.GALAXYBOT, e);
     process.exit(1);
 });
 
+async function status() {
+    await axios.post(`${config.status.kumaBase}/api/push/${config.status.id}?status=up&msg=OK&ping=42`)
+}
